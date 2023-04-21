d1efd07ff3a0144b3c17b4b2e869dd65a9e4fae8
Pedro Luiz R. de Souza <pedro.r.souza212@gmai.com>
2023-04-20 21:46:46 -0300

    Atualização dia 20-04-022

---
+++ pages/filmes/[id].js
@@ -0,0 +1,61 @@
+import Pagina from '@/components/Pagina'
+import apiFilmes from '@/services/apiFilmes'
+import React from 'react'
+import { Card, Col, Row } from 'react-bootstrap'
+
+const Detalhes = (props) => {
+  return (
+    <>
+      <Pagina titulo={props.filme.title}>
+        <Row>
+          <Col md={3}>
+            <Card.Img variant="top" src={"https://image.tmdb.org/t/p/w500" + props.filme.poster_path} />
+          </Col>
+
+          <Col md={9}>
+            <p><strong>Orçamento: </strong> {props.filme.budget}</p>
+            <p><strong>Data: </strong> {props.filme.release_date}</p>
+            <p><strong>Tempo: </strong> {props.filme.runtime} min</p>
+            <p><strong>Avaliação: </strong> {props.filme.vote_average}</p>
+
+            <div>
+              <p><strong>Gêneros: </strong></p>
+              <ul>
+                {props.filme.genres.map(item => (
+                  <li>{item.name}</li>
+                ))}
+              </ul>
+            </div>
+
+          </Col>
+        </Row>
+
+        <h2>Atores</h2>
+        <Row>
+          {props.atores.map(item => (
+            <Col md={2} className="mb-3">
+              <Card.Img variant="top" src={"https://image.tmdb.org/t/p/w500" + item.profile_path} title={item.name} />
+            </Col>
+          ))}
+        </Row>
+      </Pagina>
+    </>
+  )
+}
+
+export default Detalhes
+
+export async function getServerSideProps(context) {
+
+  const id = context.params.id
+
+  const resultado = await apiFilmes.get('/movie/' + id)
+  const filme = resultado.data
+
+  const getAtores = await apiFilmes.get('/movie/' + id + "/credits")
+  const atores = getAtores.data.cast
+
+  return {
+    props: { filme, atores },
+  }
+}
--- services/apiFilmes.js
+++ services/apiFilmes.js
@@ -1,9 +1,10 @@
+import token from "@/configTolken";
 import axios from "axios";
 
 const apiFilmes = axios.create({
     baseURL: 'https://api.themoviedb.org/3',
     headers: {
-        Authorization: 'Bearer '
+        Authorization: 'Bearer ' + token
     }
 })
 
